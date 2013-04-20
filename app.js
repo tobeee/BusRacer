af80489fe95fc0891af15e5040eb21e5382e6a4d
Toby G <tobeee@gmail.com>
2013-04-20 18:15:41 +0100

    Final Server Push
    Moved to Express middleware

--- app.js
+++ app.js
@@ -1,39 +1,20 @@
-// Including libraries
-/*
-var app = require('http').createServer(handler),
+var express = require("express"),
+    app     = express(),
+    port    = parseInt(process.env.PORT, 10) || 4570;
     
-    static = require('node-static'); // for serving files
+app.get("/", function(req, res) {
+  res.redirect("/index.html");
+});
 
-// This will make all the files in the current folder
-// accessible from the web
-var fileServer = new static.Server('./');
+app.configure(function(){
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(express.static(__dirname + '/'));
+  app.use(express.errorHandler({
+    dumpExceptions: true, 
+    showStack: true
+  }));
+  app.use(app.router);
+});
 
-// This is the port for our web server.
-// you will need to go to http://localhost:8080 to see it
-app.listen(8090);
-
-// If the URL of the socket server is opened in a browser
-function handler (request, response) {
-
-    request.addListener('end', function () {
-        fileServer.serve(request, response); // this will return the correct file
-    });
-}
-
-*/
-
-var static = require('node-static');
-
-//
-// Create a node-static server instance to serve the './public' folder
-//
-var file = new(static.Server)();
-
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        //
-        // Serve files!
-        //
-        file.serve(request, response);
-    });
-}).listen(82);
+app.listen(port);
